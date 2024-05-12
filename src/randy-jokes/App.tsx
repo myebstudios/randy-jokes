3494a5d13a51a700ef0a4c45119cf44153ea9b90
Esoe B.Studios <myebstudios@gmail.com>
2024-05-12 23:08:05 +0100

    Added GlobalStyles {AppColors, CardColors, Spaces, Typography}

--- src/randy-jokes/App.tsx
+++ src/randy-jokes/App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {AppColors, CardColors, Spaces, Typography} from './globals/GlobalStyles';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={Typography.largeHeading}>Home</Text>
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -13,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: AppColors.primary,
     alignItems: 'center',
     justifyContent: 'center',
   },
