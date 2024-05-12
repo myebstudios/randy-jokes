4603eae9fec25e77dc339f713b239cb8d1cf4762
Esoe B.Studios <myebstudios@gmail.com>
2024-05-12 23:07:31 +0100

    Added GlobalStyles {AppColors, CardColors, Spaces, Typography}

---
+++ src/randy-jokes/globals/GlobalStyles.tsx
@@ -0,0 +1,31 @@
+import {StyleSheet} from "react-native";
+
+export const AppColors = {
+        primary: '#0C1123',
+        secondary: '#1A90D2',
+        accent: '#3E61E3',
+        black: '#0C1123',
+        white: '#fff'
+}
+
+export const CardColors = [
+    '#D2AA1A',
+    '#B81AD2',
+    '#1AD2A6'
+]
+
+export const Typography = StyleSheet.create({
+    largeHeading: {
+        fontSize: 34,
+        fontWeight: 'bold',
+        color: AppColors.white
+    }
+})
+
+export const Spaces = {
+    s8: 8,
+    s16: 16,
+    s24: 24,
+    s32: 32,
+    appMargin: 20
+}
